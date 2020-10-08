d2fe5015d1437d8c53fc7994b5f194df2c9e4d9e
Nick Geerts <njgeerts@gmail.com>
2020-10-08 17:18:31 +0200

    Update PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -34,7 +34,7 @@ module.exports = {
       path,
       ssh_options: 'ForwardAgent=yes',
       'post-deploy':
-        'yarn install --prod && yarn rw build && yarn rw db up && yarn rw db seed && pm2 reload ecosystem.config.js --env production',
+        'yarn install && yarn rw build && yarn rw db up && yarn rw db seed && pm2 reload ecosystem.config.js --env production',
     },
   },
 }
