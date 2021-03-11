147ca765af24de4773b6e7a8a77757a6442f847c
Nick Geerts <info>
2021-03-11 11:31:00 +0100

    Upgrade to Redwood 0.26.0

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import { FatalErrorBoundary } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+
+import Routes from 'src/Routes'
+
+import './index.css'
+
+export default function App() {
+  return (
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodApolloProvider>
+        <Routes />
+      </RedwoodApolloProvider>
+    </FatalErrorBoundary>
+  )
+}
